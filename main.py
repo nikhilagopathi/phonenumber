605955150ebda112eda411539aeadaf9cfcc4cca
nikhilagopathi <nikhilagopathi@gmail.com>
2021-09-20 17:29:37 +0200

    add

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ print(geocoder.description_for_number(ch_number, "en"))
 from phonenumbers import carrier
 service_number = phonenumbers.parse(number, "RO")
 print(carrier.name_for_number(service_number, "en"))
+print /("hello, Thank you ")
